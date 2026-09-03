6cee1e0922b508dae14326dc60d294da5c40727c
OscarFernandezUAIusuario <oscar.fernandez@alumnos.uai.edu.ar>
2026-09-03 17:55:18 -0300

    Sumo Interfaces y clases genéricas

---
+++ BE_517OF/EntidadAuditable_517OF.cs
@@ -0,0 +1,9 @@
+﻿namespace BE_517OF
+{
+    public abstract class EntidadAuditable_517OF : EntidadBase_517OF
+    {
+        public DateTime FechaCreacion_517OF { get; set; }
+        public DateTime? FechaEliminacion_517OF { get; set; }
+        public string DVH_517OF { get; set; } = string.Empty;
+    }
+}
